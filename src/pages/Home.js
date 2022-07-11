f827c1cd4c44c31b702c7d07f54e7e65883ad196
praveen <2pkashyap2001@gmail.com>
2022-07-11 20:36:30 +0530

    add fetch from tvmaze and search function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
-  return (
-    <MainPageLayout>This is home page</MainPageLayout>
-  )
+     // eslint-disable-next-line 
+  const[input,setInput]=useState('');
+  
+  const OnInputChange =(ev) => {
+      setInput(ev.target.value);
+  }
+  const onSearch=()=>{
+       fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
+               console.log(result);
+           }
+       )
+  }
+
+  const onKeyDown=(ev)=>
+  { 
+      if(ev.keyCode===13){
+          onSearch( )
+      }
+
+  }
+
+    return (
+    <MainPageLayout>
+        <input type="text" onChange={OnInputChange} onKeyDown={onKeyDown} value={input}/>
+    <button type="button" onClick={onSearch}>
+        Search</button>
+    </MainPageLayout>
+  )  
 }
 
 export default Home
