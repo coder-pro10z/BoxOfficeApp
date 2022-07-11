f301df9d89c9744dea561c813474c97b0429a60c
praveen <2pkashyap2001@gmail.com>
2022-07-11 21:18:30 +0530

    added API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,19 @@
+
 import React, { useState } from 'react'
 import MainPageLayout from '../components/MainPageLayout'
 
 const Home = () => {
      // eslint-disable-next-line 
-  const[input,setInput]=useState('');
-  
+  const [input,setInput]=useState('');
+  const [results,setResults]=useState(null);
+
   const OnInputChange =(ev) => {
       setInput(ev.target.value);
   }
-  const onSearch=()=>{
+  const onSearch=()=>{ 
        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(r=>r.json()).then(result=>{
-               console.log(result);
+
+               setResults(result);
            }
        )
   }
@@ -20,14 +23,29 @@ const Home = () => {
       if(ev.keyCode===13){
           onSearch( )
       }
-
   }
+
+  const renderResults=()=>{
+      if(results && results.length === 0){
+return <div>NO RESULTS</div>
+      }
+      if(results && results.length > 0){
+          return <div> 
+              {results.map(item=>
+          <div key={item.show.id}>{item.show.name}</div>)} 
+          </div>
+          
+    }
+    return null;
+
+    }
 
     return (
     <MainPageLayout>
         <input type="text" onChange={OnInputChange} onKeyDown={onKeyDown} value={input}/>
     <button type="button" onClick={onSearch}>
         Search</button>
+        {renderResults()}
     </MainPageLayout>
   )  
 }
