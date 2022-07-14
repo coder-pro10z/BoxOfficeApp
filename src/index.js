fcc3c43fe9eef2c6119282c35b2888bd8c673be2
praveen <2pkashyap2001@gmail.com>
2022-07-14 12:01:59 +0530

    deployed and add hash routing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter>
+<HashRouter>
 <App />
-</BrowserRouter>
+</HashRouter>
 
 );
 
